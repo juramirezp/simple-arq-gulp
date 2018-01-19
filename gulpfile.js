2917b1447c9a8a85579460a264d04c56d5f9311b
Juan Ramirez <jramirez@interside.cl>
2018-01-19 20:58:51 -0300

    Configuracion gulp-uglify

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,16 @@ const gulp = require('gulp');
 // const gulpSass = require('gulp-sass');
 const gulpStylus = require('gulp-stylus');
 const gulpAutoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
 
 gulp.task('css', ()=>
     gulp.src('./src/scss/**/*.styl')
         .pipe(gulpStylus())
         .pipe(gulp.dest('./dist/css/'))
 );
+
+gulp.task('js', function() {
+    return gulp.src('./src/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js/'))
+});
